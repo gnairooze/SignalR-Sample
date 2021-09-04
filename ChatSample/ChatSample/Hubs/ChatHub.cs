2ff1aaba013d71368b52d16b89b9fc95dfc6f456
George Nairooze <george.nairooze@linkdev.com>
2021-09-04 21:48:16 +0200

    first commit

---
+++ ChatSample/ChatSample/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatSample.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string name, string message)
+        {
+            string fullName = $"[{DateTime.Now.ToString("MMM-dd HH:mm:ss")}].[{UserHandler.ConnectedIds.Count}].|.{name}";
+            // Call the broadcastMessage method to update clients.
+            await Clients.All.SendAsync("broadcastMessage", fullName, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ ChatSample/ChatSample/Hubs/UserHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatSample.Hubs
+{
+    public static class UserHandler
+    {
+        public static HashSet<string> ConnectedIds = new HashSet<string>();
+    }
+}
